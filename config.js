70fe1000190db1562a1c7205cc4b873a471aebe9
Linda van den Brink <l.vandenbrink@geonovum.nl>
2017-08-25 11:35:38 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "ST",
+  publishDate: "2017-08-25",
+  editors: [{
+    name: "First Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  },{
+    name: "Second Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  }],
+  shortName: "nldp",
+  pubDomain: "nen3610",
+  github: "https://github.com/Geonovum/NEN3610-Linkeddata",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
