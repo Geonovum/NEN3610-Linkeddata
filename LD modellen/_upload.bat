e1b68db864a8f60d9cee02f504ec97c682619513
architolk <msj@xs4all.nl>
2018-01-29 22:33:16 +0100

    Updated golf course example

--- LD modellen/_upload.bat
+++ LD modellen/_upload.bat
@@ -5,4 +5,6 @@ curl.exe -X PUT -T "nen3610 referenties.ttl" http://localhost:8080/nen3610/conta
 curl.exe -X PUT -T "nen3610 vocabulaire.ttl" http://localhost:8080/nen3610/container/vocabulaire
 curl.exe -X PUT -T "nen3610 shapes.ttl" http://localhost:8080/nen3610/container/structuur
 curl.exe -X PUT -T "IM Golf.ttl" http://localhost:8080/nen3610/container/imgolf
+curl.exe -X PUT -T "Voorbeeld golfbaan.ttl" http://localhost:8080/nen3610/container/golfbaan
+curl.exe -X POST -T "Voorbeeld golfbaan details.ttl" http://localhost:8080/nen3610/container/golfbaan
 pause
