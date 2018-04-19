9a589a5aedf8eea3e0070dfff077b63b87402289
Pano Maria <pano.maria@kadaster.nl>
2018-04-19 23:06:18 +0200

    WIP commit

--- config.js
+++ config.js
@@ -25,4 +25,21 @@ var respecConfig = {
   //    "publisher": "International Organization for Standardization",
   //  },
   //},
+  localBiblio: {
+    NEN3610: {
+      id: "NEN 3610:2011 nl",
+      title: "NEN 3610:2011 nl - Basismodel geo-informatie - Termen, definities, relaties en algemene regels voor de uitwisseling van informatie over aan de aarde gerelateerde ruimtelijke objecten",
+      href: "https://www.nen.nl/NEN-Shop/Norm/NEN-36102011-nl.htm",
+      status: "Definitief",
+      publisher: "NEN",
+      date: "2011-03-01"
+    },
+    INSPIRERDF: {
+      title: "Guidelines for the RDF encoding of spatial data",
+      href: "http://inspire-eu-rdf.github.io/inspire-rdf-guidelines",
+      status: "Draft",
+      publisher: "ARE3NA project \"INSPIRE Re3ference Platform Phase 2\"",
+      date: "2017-07-17"
+    }
+  }
 };
