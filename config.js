a07816045237a7eea17e8a54e1fcf2ffb9e51094
Linda van den Brink <l.vandenbrink@geonovum.nl>
2018-08-29 09:59:41 +0200

    added issuebase

--- config.js
+++ config.js
@@ -37,6 +37,7 @@ var respecConfig = {
   shortName: "nldp",
   pubDomain: "nen3610",
   github: "https://github.com/Geonovum/NEN3610-Linkeddata",
+  issueBase: "https://github.com/Geonovum/NEN3610-Linkeddata/issues",
   //previousPublishDate: "2017-01-01",
   //previousStatus: "GN-CV",
   //localBiblio: {
